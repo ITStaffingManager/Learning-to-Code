6b1bd65dd1bf4b30793f47970402ef9f1ba1034c
Black_Napalm <stormthorjazz@gmail.com>
2023-12-10 08:40:13 -0700

    Add files via upload
    Week 1, Core assignment 1 - Creating Variables

---
+++ Avant_Grailyn_JS_CreateVariables.js
@@ -0,0 +1,24 @@
+//variables for our program to determine if a rider is allowed to ride the coaster
+//STRECTH Feature 1
+var minimum_height = 42;
+var minimum_age = 10;
+if (minimum_height >= minimum_age) 
+{
+    console.log("Welcome to the Roller Coaster");
+}
+else {
+    console.log("Sorry kiddo. Maybe next year");
+}
+
+
+
+// STRETCH Feature 2
+var minimum_height = 42;
+var minimum_age = 10;
+if (minimum_height || minimum_age) 
+{
+    console.log("Welcome to the Roller Coaster");
+}
+else {
+    console.log("Sorry kiddo. Maybe next year");
+}
