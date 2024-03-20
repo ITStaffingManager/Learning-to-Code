b4db7b8f8a737b374bdb6ae80734b938ac162c27
Black_Napalm <stormthorjazz@gmail.com>
2024-03-20 16:51:24 -0700

    Algo practice

---
+++ ALGO APP chalenges.js
@@ -0,0 +1,15 @@
+var x = [2.4.6.3.7];
+var y = x.length;
+    comsole.log(x[y-1[);
+
+
+    ]])
+
+    var x = [1,3,5,7];
+    x[x.length-3] = x[x.length/2+x.length/4];
+console.log(x)]
+
+
+
+for(var i = 8; i > 7; i +=1)
+console.log(i)
