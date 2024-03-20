8b78516007e2cfb96840b328d195cc5eed66d80a
Black_Napalm <stormthorjazz@gmail.com>
2024-03-20 16:53:38 -0700

    JS code assignment

---
+++ Avant_Grailyn_JS_code.js
@@ -0,0 +1,7 @@
+// creates a variable called 'likecount' and assigns it a value of ZERO
+var likeCount = 0;
+// creates a function called 'increaseLikes'
+function increaseLikes() {
+    // tells the variable to add 1 to its current value
+    likeCount = likeCount + 1;
+}
